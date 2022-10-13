b5662b3de7bb87fb5061f2db407659fd295c92e0
Guillaume Vigne <guillaume.vigne007@gmail.com>
2022-10-13 23:39:07 +0200

    Deploy molonaviz as a python package

---
+++ src/molonaviz/frontend/dialogAboutUs.py
@@ -0,0 +1,22 @@
+from PyQt5 import QtWidgets, uic
+from PyQt5.QtGui import QPixmap
+import os.path
+from ..utils.get_files import get_ui_asset, get_imgs
+
+From_DialogAboutUs = uic.loadUiType(get_ui_asset("dialogAboutUs.ui"))[0]
+
+class DialogAboutUs(QtWidgets.QDialog,From_DialogAboutUs):
+    """
+    Display some text and a picture about the app's creators.
+    """
+    def __init__(self):
+        super(DialogAboutUs, self).__init__()
+        QtWidgets.QDialog.__init__(self)
+
+        self.setupUi(self)
+
+        logoMines = get_imgs("LogoMines.jpeg")
+        logoMolonaviz = get_imgs("MolonavizIcon.png")
+        self.labelMolonaviz.setPixmap(QPixmap(logoMolonaviz))
+        self.labelMines.setPixmap(QPixmap(logoMines))
+
